dbcf9f9d679a34f8909abb7b1d278682f9b72c96
dblach <298039@uwr.edu.pl>
2020-06-11 22:04:00 +0200

    update

---
+++ php/wyswietl_wizyty_pacjenta.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="styles/timetablejs.css">
+	<link rel="stylesheet" href="styles/tabs.css">
+	<script src="scripts/timetable.js"></script>
+	<script src="scripts/timetable_functions.js"></script>
+	<script src="scripts/tabs.js"></script>
+</head>
+<body>
+	<?php
+		include "polaczenie.php";
+		$idp=$_GET['idp'];
+		if(!isset($_POST['dp'])){
+			$dp=date('Y-m-d');
+		}
+		else{
+			$dp=$_POST['dp'];
+		}
+		$dk=date('Y-m-d',strtotime($dp.'+ 7 days'));
+		$pacjent_imienazwisko=mysqli_fetch_array(mysqli_query($l,"select concat_ws(' ',imie,nazwisko) from pacjenci where id_pacjenta=$idp"))[0];
+		$gp=8;
+		$gk=23;
+		$dt=['','Pon ','Wt ','Śr ','Czw ','Pt ','So ','N '];
+		$z="select id_wizyty,wizyty.id_lekarza,data,time_format(godzina_rozpoczecia,\"%H:%i\") as godzina_rozpoczecia,time_format(godzina_zakonczenia,\"%H:%i\") as godzina_zakonczenia,concat_ws(' ',nazwisko,imie) as lekarz,weekday(data)+1 as dt,date_format(data,\"%e.%m\") as da from wizyty join lekarze on lekarze.id_lekarza=wizyty.id_lekarza where id_pacjenta=$idp and data>=\"$dp\" and data<=\"$dk\" order by data";
+		
+		function pobierz_wizyty_widokdnia(){
+			global $l;
+			global $z;
+			global $dt;
+			$q=mysqli_query($l,$z);
+			while($r=mysqli_fetch_assoc($q)){
+				$dd=str_replace('-',',',$r['data']);
+				$dl=$dt[$r['dt']].$r['da'];
+				$gr=substr(str_replace(':',',',$r['godzina_rozpoczecia']),0,5);
+				$gz=substr(str_replace(':',',',$r['godzina_zakonczenia']),0,5);
+				$lek=$r['lekarz'];
+				$idw=$r['id_wizyty'];
+				echo "if(!loc.includes(\"$dl\")){";
+				echo "loc.push(\"$dl\");";
+				echo "timetable.addLocations(['$dl']);";
+				echo "}";
+				echo "timetable.addEvent('$lek','$dl',new Date($dd,$gr),new Date($dd,$gz));\n";
+				echo "ids.push(\"$idw\");";
+			}
+		}
+		
+		function pobierz_wizyty_lista(){
+			global $l;
+			global $z;
+			global $dt;
+			global $idp;
+			global $dp;
+			$q=mysqli_query($l,$z);
+			
+			while($r=mysqli_fetch_assoc($q)){
+				$gr=$r['godzina_rozpoczecia'];
+				$gz=$r['godzina_zakonczenia'];
+				$idl=$r['id_lekarza'];
+				$idw=$r['id_wizyty'];
+				$data=$r['data'];
+				$pq=mysqli_query($l,"select nazwa_poradni from terminy_przyjec where id_lekarza=$idl and dzien_tygodnia=weekday(\"$data\")+1 and godzina_otwarcia<=\"$gr\" and godzina_zamkniecia>=\"$gz\"");
+				if(mysqli_num_rows($pq)==0) $p='?'; else $p=mysqli_fetch_array($pq)[0];
+				
+				echo "<tr>";
+				echo "<td>".$dt[$r['dt']].$r['da']."</td>";
+				echo "<td>$gr - $gz</td>";
+				echo "<td>".$r['lekarz']."</td>";
+				echo "<td>$p</td>";
+				echo '<td><a href="edytuj_wizyte.php?idw='.$r['id_wizyty']."&target=wyswietl_wizyty_pacjenta.php?idp=$idp&dp=$dp\">Edytuj</a></td>";
+				echo "</tr>";
+			}
+		}
+	?>
+
+	<a href="index.php">Strona główna</a>
+	&nbsp;&gt;&nbsp;
+	Zarządzanie pacjentami
+	&nbsp;&gt;&nbsp;
+	<a href="wybierz_pacjenta.php?target=wyswietl_wizyty_pacjenta.php?">Edytuj wizyty pacjentów</a>
+	&nbsp;&gt;&nbsp;
+	Wizyty pacjenta
+	
+	<hr>
+	
+	<h2>Wizyty pacjenta: <?php echo $pacjent_imienazwisko;?></h2>
+	<br>
+	<form action="wyswietl_wizyty_pacjenta.php?&idp=<?php echo $idp;?>" method="post">
+		Okres: od <input type="date" name="dp" value="<?php echo $dp;?>"/>
+		&nbsp;
+		do: <input type="date" name="dk" disabled="true" value="<?php echo $dk;?>"/>
+		&nbsp;
+		<input type="submit" value="Zmień"/>
+	</form>
+
+	<hr>
+	
+	<div class="tab">
+		<button class="tablinks" onclick="changeTab(event,'Widoktygodnia')" id="defaultOpen">Widok tygodnia</button>
+		<button class="tablinks" onclick="changeTab(event,'Lista')">Lista</button>
+	</div>
+	
+	<div id="Widoktygodnia" class="tabcontent">
+		<div class="timetable"></div>
+		<script>
+			var timetable=new Timetable();
+			var loc=[];
+			var ids=[];
+			timetable.setScope(<?php echo "$gp,$gk";?>);
+			<?php pobierz_wizyty_widokdnia(); ?>
+			var renderer=new Timetable.Renderer(timetable);
+			renderer.draw('.timetable');
+			add_onclick("&target='wyswietl_wizyty_pacjenta.php?dp=<?php echo $dp;?>&idp=<?php echo $idp;?>'");
+		</script>
+	</div>
+	
+	<div id="Lista" class="tabcontent">
+		<table border="1">
+		<tr>
+			<th>Data</th>
+			<th>Godziny</th>
+			<th>Lekarz</th>
+			<th>Poradnia</th>
+		</tr>
+		<?php pobierz_wizyty_lista();?>
+		</table>
+	</div>
+	
+	<script>
+		document.getElementById("defaultOpen").click();
+	</script>
+</body>
